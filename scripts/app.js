ba02215df478dacf0a03a91269ded8bde6b25c0e
Mariano Méndez <marianoezequielmendez@gmail.com>
2023-04-20 21:15:08 -0300

    V0.0.0

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll(".section"),
+  sectBtns = document.querySelectorAll(".controlls"),
+  sectBtn = document.querySelectorAll(".control"),
+  allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+}
+
+PageTransitions();
