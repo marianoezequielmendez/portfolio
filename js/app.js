b8d54855e56b6f13d1f35f49e33ea0b51164046e
Mariano Méndez <marianoezequielmendez@gmail.com>
2023-04-23 19:47:02 -0300

    contact

--- js/app.js
+++ js/app.js
@@ -36,6 +36,13 @@ function PageTransitions() {
       element.classList.add("active");
     }
   });
+
+  //Toggle Theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 PageTransitions();
